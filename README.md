fbaa1149f9ec943264b3f5c796d7298fca84a019
Lyridsss <trungtincom7@gmail.com>
2023-11-23 03:50:35 +0700

    config

--- README.md
+++
@@ -1,4 +0,0 @@
-bg-gray-100
-text-gray-900
-text-gray-600
-text-purple-600
--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,2 +1,1 @@
-export const server = "http://localhost:8080"
-// export const server = "https://smartphone-store-cceab5b8f896.herokuapp.com/"
+export const server = "http://smartphone-store-web-app.ap-southeast-1.elasticbeanstalk.com"
