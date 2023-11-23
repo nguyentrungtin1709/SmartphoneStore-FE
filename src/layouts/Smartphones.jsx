f5a127eb09eaa9ea40be215602490fcf21fa3745
Lyridsss <trungtincom7@gmail.com>
2023-11-23 16:52:20 +0700

    a to Link

--- src/layouts/Smartphones.jsx
+++ src/layouts/Smartphones.jsx
@@ -401,7 +401,7 @@ function Smartphones(){
                                                     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                                                         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                                                             {products.map((smartphone) => (
-                                                                <a key={smartphone.id} href={`/smartphones/${smartphone.id}`} className="group border px-2 py-2 rounded-md">
+                                                                <Link key={smartphone.id} to={`/smartphones/${smartphone.id}`} className="group border px-2 py-2 rounded-md">
                                                                     <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                                                                         <img
                                                                             src={smartphone.imageUrl}
@@ -411,7 +411,7 @@ function Smartphones(){
                                                                     </div>
                                                                     <h3 className="mt-4 h-12 text-sm text-gray-700">{smartphone.name}</h3>
                                                                     <p className="mt-1 text-lg font-medium text-gray-900">{getPrice(smartphone.price)} đ</p>
-                                                                </a>
+                                                                </Link>
                                                             ))}
                                                         </div>
                                                     </div>
